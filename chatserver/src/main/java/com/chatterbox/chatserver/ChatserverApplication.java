829210e5c0b5ffa1c337b1f664a944d6bb864d5b
ag-shubham <shubhamsatnalika64@gmail.com>
2024-02-11 00:23:56 +0530

    minor improvements

--- chatserver/src/main/java/com/chatterbox/chatserver/ChatserverApplication.java
+++ chatserver/src/main/java/com/chatterbox/chatserver/ChatserverApplication.java
@@ -2,12 +2,20 @@ package com.chatterbox.chatserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ChatserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatserverApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String home() {
+        return "Initializing server";
+    }
 
 }
